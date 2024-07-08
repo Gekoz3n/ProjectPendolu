b29e36952e2c782b91bc3927e5f95f38f15acac6
Gekoz3n <69292115+Gekoz3n@users.noreply.github.com>
2024-07-08 05:45:45 -0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const Event = require('../structures/event.js');
 
 module.exports = new Event('ready', client => {
-	console.log('--------- Co-Pilot is ready! ---------');
+	console.log('--------- PendoluHelper is ready! ---------');
 	client.user.setActivity(`${client.prefix}help`, { type: 'LISTENING' });
 });
